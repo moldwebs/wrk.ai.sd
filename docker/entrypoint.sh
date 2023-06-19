f0d80132d67cfa9fc44031f81fd75a14972e105e
Roman <moldwebs@gmail.com>
2023-06-19 18:03:10 +0300

    UPD

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,6 +12,7 @@ MOUNTS["${ROOT}/embeddings"]="${VOLUME_SPACE}/embeddings"
 MOUNTS["${ROOT}/extensions"]="${VOLUME_SPACE}/extensions"
 MOUNTS["${ROOT}/scripts"]="${VOLUME_SPACE}/scripts"
 
+mkdir -vp ${VOLUME_SPACE}/models/Stable-diffusion ${VOLUME_SPACE}/models/ControlNet ${VOLUME_SPACE}/models/Codeformer ${VOLUME_SPACE}/models/GFPGAN ${VOLUME_SPACE}/models/ESRGAN ${VOLUME_SPACE}/models/BSRGAN ${VOLUME_SPACE}/models/RealESRGAN ${VOLUME_SPACE}/models/SwinIR ${VOLUME_SPACE}/models/LDSR ${VOLUME_SPACE}/models/ScuNET ${VOLUME_SPACE}/models/VAE ${VOLUME_SPACE}/models/Deepdanbooru ${VOLUME_SPACE}/models/midas ${VOLUME_SPACE}/models/Lora ${VOLUME_SPACE}/models/hypernetworks ${VOLUME_SPACE}/models/torch_deepdanbooru ${VOLUME_SPACE}/models/BLIP ${VOLUME_SPACE}/models/openpose
 
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
